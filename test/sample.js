fec1f39bdf0b9ad3cdb2edcec43b2998a7f04445
Marshall Bowers <elliott.codes@gmail.com>
2014-11-29 16:41:03 -0500

    Add sample update, delete, and info operations

--- test/sample.js
+++ test/sample.js
@@ -4,5 +4,9 @@ var OlegDB = require('../lib/driver');
 
 var db = new OlegDB('localhost', 38080, 'turtles');
 
-db.create('red', 'Raphael');
+db.create('red', 'Michaelangelo');
 db.read('red');
+db.update('red', 'Raphael');
+db.read('red');
+db.delete('red');
+db.info('red');
