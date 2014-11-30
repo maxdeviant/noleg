7a2a4c0ac0d355d10c108bb95b2c02442b742c8f
Marshall Bowers <elliott.codes@gmail.com>
2014-11-30 12:19:13 -0500

    Pass status code to callback

--- test/sample.js
+++ test/sample.js
@@ -6,6 +6,6 @@ oleg.connect('http://localhost:38080/turtles');
 
 var turtle = oleg.pair('red', 'Raphael');
 
-turtle.save(function (data) {
-    console.log(data);
+turtle.save(function (data, status) {
+    console.log(status + ': ' + data);
 });
