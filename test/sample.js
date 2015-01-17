c1318728da04ac237eeda57a8724727f0e424ab0
Marshall Bowers <elliott.codes@gmail.com>
2015-01-16 20:11:58 -0500

    Replace pairs with models

--- test/sample.js
+++ test/sample.js
@@ -4,8 +4,15 @@ var oleg = require('../');
 
 oleg.connect('http://localhost:38080/turtles');
 
-var turtle = oleg.pair('red', 'Raphael');
+var turtle = oleg.model({
+    name: 'Raphael',
+    color: 'red'
+});
 
-turtle.save(function (data, status) {
-    console.log(status + ': ' + data);
+turtle.save(function (data, status, responseText) {
+    console.log(data, status, responseText);
+});
+
+turtle.find('2c62b0a8-e4e7-4218-99dc-eccc1099088a', function (data, status) {
+    console.log(data);
 });
